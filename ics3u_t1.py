22d07bcfb6eaa2b64e5503567f34ec8c611fc4d3
David Wood <david.wood@GCCYK6ZXJ1WK.local>
2019-09-16 08:24:38 -0400

    update

---
+++ ics3u_t1.py
@@ -0,0 +1,26 @@
+# This program calculates the average of three numbers.
+# Author: David Wood
+# Date: September 12 2019
+# Upper Canada College
+# White = variables (holds the place of something)
+# Blue = keywords / reserved word
+# Yellow = information that shows up on the screen
+# Red = concatination 
+# String = sentence
+# Int = number
+
+number1 = input("What is your first number?")
+print("Your first number is " + number1)
+
+number2 = input("What is your second number?")
+print("Your second number is " + number2)
+
+number3 = input("What is your third number?")
+print("Your third number is " + number3)
+
+number4 = input("What is your fourth number?")
+print("Your third number is" + number4)
+
+average = (int(number1) + int(number2) + int(number3) + int(number4))/4
+
+print("The average is " + str(average))
